b64c2198bd875bf99e050cf480a85ed6722a9417
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-04-08 13:33:21 +0400

    accounts changed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Cavid Abasov',
+  owner: 'Tofig Amraslanov',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -25,7 +25,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Tofig Amraslanov',
+  owner: 'Cavid Abasov',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
